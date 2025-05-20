0da274cf71fe3868b2ae2ac64f9dbeef81247dd7
FormulaV <jackysaktilim@gmail.com>
2025-05-20 13:57:17 +0700

    p

---
+++ Forex/app/src/main/java/com/j222102450/forex/RootModel.java
@@ -0,0 +1,17 @@
+package com.j222102450.forex;
+
+import com.google.gson.annotations.SerializedName;
+public class RootModel {
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+
+    public RootModel() {}
+
+    public RatesModel getRatesModel() {
+        return ratesModel;
+    }
+
+    public void setRatesModel(RatesModel ratesModel) {
+        this.ratesModel = ratesModel;
+    }
+}
